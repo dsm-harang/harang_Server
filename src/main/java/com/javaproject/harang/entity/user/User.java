452896d99a5958a4d7e97062537911988e1412d6
Choi ha yeon <chlgky82@gmail.com>
2020-10-15 15:28:24 +0900

    [ADD] - user interface

---
+++ src/main/java/com/javaproject/harang/entity/user/User.java
@@ -0,0 +1,15 @@
+package com.javaproject.harang.entity.user;
+
+import com.javaproject.harang.security.AuthorityType;
+
+public interface User {
+    Integer getId();
+    String getUserId();
+    String getPassword();
+    String getName();
+    Integer getAge();
+    Integer getPhoneNumber();
+    String getImagePath();
+    String getIntro();
+    AuthorityType getType();
+}
