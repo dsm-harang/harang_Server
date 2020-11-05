236b66980f704c5bf4a92afd6c8a4da074876d73
Choi ha yeon <chlgky82@gmail.com>
2020-11-05 21:44:40 +0900

    [FIX] - entity repository

--- src/main/java/com/javaproject/harang/entity/Post/PostRepository.java
+++ src/main/java/com/javaproject/harang/entity/Post/PostRepository.java
@@ -1,8 +1,12 @@
 package com.javaproject.harang.entity.Post;
 
+import com.javaproject.harang.entity.user.customer.Customer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends CrudRepository<Post, Integer> {
+    Optional<Post> deleteByUserId(Integer userId);
 }
--- src/main/java/com/javaproject/harang/entity/member/MemberRepository.java
+++ src/main/java/com/javaproject/harang/entity/member/MemberRepository.java
@@ -1,8 +1,13 @@
 package com.javaproject.harang.entity.member;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.expression.spel.ast.OpAnd;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends CrudRepository<Member, Integer> {
+    Optional<Member> findByUserIdAndPostId(Integer userId, Integer postId);
+    Integer countAllByPostId(Integer postId);
 }
