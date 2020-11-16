45b05bded08c6c7d41963d625de9ec2e47cde219
Choi ha yeon <chlgky82@gmail.com>
2020-11-16 14:28:21 +0900

    [FIX] - admin controller

--- src/main/java/com/javaproject/harang/controller/AdminController.java
+++ src/main/java/com/javaproject/harang/controller/AdminController.java
@@ -1,52 +1,58 @@
-//package com.javaproject.harang.controller;
-//
-//import com.javaproject.harang.payload.response.PostReportResponse;
-//import com.javaproject.harang.payload.response.UserReportResponse;
-//import com.javaproject.harang.service.admin.AdminService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/admin")
-//@RequiredArgsConstructor
-//public class AdminController {
-//
-//    private final AdminService adminService;
-//
-//    @DeleteMapping("/user/{userId}")
-//    public void userDelete(@PathVariable Integer userId){
-//        adminService.userDelete(userId);
-//    }
-//
-//    @DeleteMapping("post/{postId}")
-//    public void userPostDelete(@PathVariable Integer postId) {
-//        adminService.userPostDelete(postId);
-//    }
-//
-//    @DeleteMapping("report/user/{userId}")
-//    private void userReportDelete(@PathVariable Integer userId) {
-//        adminService.userReportDelete(userId);
-//    }
-//
-//    @DeleteMapping("report/post/{postId}")
-//    private void postReportDelete(@PathVariable Integer postId) {
-//        adminService.postReportDelete(postId);
-//    }
-//
-//    @DeleteMapping("score/{userId}")
-//    private void score(@PathVariable Integer userId) {
-//        adminService.score(userId);
-//    }
-//
-//    @GetMapping("post")
-//    public List<PostReportResponse> postReport() {
-//        return adminService.postReport();
-//    }
-//
-//    @GetMapping("user")
-//    public List<UserReportResponse> userReport() {
-//        return adminService.userReport();
-//    }
-//}
+package com.javaproject.harang.controller;
+
+import com.javaproject.harang.payload.response.PostReportResponse;
+import com.javaproject.harang.payload.response.UserPageResponse;
+import com.javaproject.harang.payload.response.UserReportResponse;
+import com.javaproject.harang.service.admin.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @DeleteMapping("/user/{userId}")
+    public void userDelete(@PathVariable Integer userId){
+        adminService.userDelete(userId);
+    }
+
+    @DeleteMapping("post/{postId}")
+    public void userPostDelete(@PathVariable Integer postId) {
+        adminService.userPostDelete(postId);
+    }
+
+    @DeleteMapping("report/user/{userId}")
+    private void userReportDelete(@PathVariable Integer userId) {
+        adminService.userReportDelete(userId);
+    }
+
+    @DeleteMapping("report/post/{postId}")
+    private void postReportDelete(@PathVariable Integer postId) {
+        adminService.postReportDelete(postId);
+    }
+
+    @DeleteMapping("score/{userId}")
+    private void score(@PathVariable Integer userId) {
+        adminService.score(userId);
+    }
+
+    @GetMapping("post")
+    public List<PostReportResponse> postReport() {
+        return adminService.postReport();
+    }
+
+    @GetMapping("user")
+    public List<UserReportResponse> userReport() {
+        return adminService.userReport();
+    }
+
+    @GetMapping("/{userId}")
+    public UserPageResponse userPage(@PathVariable Integer userId){
+        return adminService.userPage(userId);
+    }
+}
