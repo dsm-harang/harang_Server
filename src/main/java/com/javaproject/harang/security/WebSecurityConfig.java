dbc6b0546d6ffd8a25767bf2e9f659f3cc091c6b
Ryua <machiro119@naver.com>
2020-11-20 21:39:47 +0900

    [FIX] - add cors

--- src/main/java/com/javaproject/harang/security/WebSecurityConfig.java
+++ src/main/java/com/javaproject/harang/security/WebSecurityConfig.java
@@ -30,6 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
     protected void configure(HttpSecurity http) throws Exception{
         http
                 .csrf().disable()
+                .cors().and()
                 .formLogin().disable()
                 .authorizeRequests()
                 .antMatchers("/auth").permitAll()
