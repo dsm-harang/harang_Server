e366dbe8d465268720e85196e7232c55288aa7bf
Ryua <machiro119@naver.com>
2020-11-20 20:02:07 +0900

    [FIX] - chat

--- src/main/java/com/javaproject/harang/controller/ChatMessageController.java
+++ src/main/java/com/javaproject/harang/controller/ChatMessageController.java
@@ -1,22 +1,22 @@
-package com.javaproject.harang.controller;
-
-import com.javaproject.harang.payload.response.chatResponse.ChatMessageForm;
-import com.javaproject.harang.service.chat.ChatMessageService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
-
-@Controller
-@RequiredArgsConstructor
-public class ChatMessageController {
-    private final SimpMessagingTemplate simpMessagingTemplate;
-    private final ChatMessageService chatMessageService;
-    @MessageMapping("/chat/send")
-    public void sendMsg(ChatMessageForm message) {
-        Integer chatRoomId = message.getChatRoomId();
-        chatMessageService.save(message);
-        simpMessagingTemplate.convertAndSend("/topic/" + chatRoomId,message);
-    }
-
-}
+//package com.javaproject.harang.controller;
+//
+//import com.javaproject.harang.payload.response.chatResponse.ChatMessageForm;
+//import com.javaproject.harang.service.chat.ChatMessageService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.messaging.simp.SimpMessagingTemplate;
+//import org.springframework.stereotype.Controller;
+//
+//@Controller
+//@RequiredArgsConstructor
+//public class ChatMessageController {
+//    private final SimpMessagingTemplate simpMessagingTemplate;
+//    private final ChatMessageService chatMessageService;
+//    @MessageMapping("/chat/send")
+//    public void sendMsg(ChatMessageForm message) {
+//        Integer chatRoomId = message.getChatRoomId();
+//        chatMessageService.save(message);
+//        simpMessagingTemplate.convertAndSend("/topic/" + chatRoomId,message);
+//    }
+//
+//}
