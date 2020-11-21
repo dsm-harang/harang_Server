0859cf5b0490d96de8b6fd52c7a9f4fcaadd768a
Ryua <machiro119@naver.com>
2020-11-21 21:59:30 +0900

    [FIX] - my mistake

--- src/main/java/com/javaproject/harang/security/WebSecurityConfig.java
+++ src/main/java/com/javaproject/harang/security/WebSecurityConfig.java
@@ -37,7 +37,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
                 .antMatchers("/auth").permitAll()
                 .antMatchers("/user/**").permitAll()
                 .antMatchers("/post/**").permitAll()
-                .antMatchers("/admin/**").hasRole("ADMIN")
                 .and()
                 .apply(new JwtConfigurer(jwtProvider)).and();
     }
