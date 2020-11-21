2bcc7f71bce22c6f2605ad2f1643adf562d6b771
Ryua <machiro119@naver.com>
2020-11-21 14:56:13 +0900

    [FIX] - add permission denied exception

--- src/main/java/com/javaproject/harang/exception/PermissionDeniedException.java
+++ src/main/java/com/javaproject/harang/exception/PermissionDeniedException.java
@@ -1,4 +1,10 @@
 package com.javaproject.harang.exception;
 
-public class PermissionDenied extends {
+import com.javaproject.harang.error.BusinessException;
+import com.javaproject.harang.error.ErrorCode;
+
+public class PermissionDeniedException extends BusinessException {
+    public PermissionDeniedException() {
+        super(ErrorCode.PERMISSION_DENIED_EXCEPTION);
+    }
 }
