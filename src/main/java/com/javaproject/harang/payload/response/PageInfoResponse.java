69f5806cae53539a449f758204857f749477b39f
Ryua <machiro119@naver.com>
2020-11-22 15:33:47 +0900

    [ADD] - page info response

--- src/main/java/com/javaproject/harang/payload/response/PageInfoResponse.java
+++ src/main/java/com/javaproject/harang/payload/response/PageInfoResponse.java
@@ -1,2 +1,13 @@
-package com.javaproject.harang.payload.response;public class PageInfoResponse {
+package com.javaproject.harang.payload.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PageInfoResponse {
+    private Integer id;
+    private String name;
+    private String intro;
+    private String imagName;
 }
