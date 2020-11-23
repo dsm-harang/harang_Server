eb6c694f8003adedb66601d36b82a0aee8489db4
Ryua <machiro119@naver.com>
2020-11-23 17:59:59 +0900

    [ADD] - message room repository

---
+++ src/main/java/com/javaproject/harang/entity/message/repository/MessageRoomRepository.java
@@ -0,0 +1,12 @@
+package com.javaproject.harang.entity.message.repository;
+
+import com.javaproject.harang.entity.message.MessageRoom;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MessageRoomRepository extends CrudRepository<MessageRoom, Integer> {
+    Optional<MessageRoom> findByPostId(Integer postId);
+}
